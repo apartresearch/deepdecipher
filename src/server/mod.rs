0604de0ade9a12a974c1090ed10fcf79fd5646fc
Albert Garde <albertsgarde@gmail.com>
2023-06-10 17:16:49 +0200

    :sparkles: Create basic first API

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,8 +1,18 @@
-use actix_web::{get, rt, App, HttpServer, Responder};
+use std::path::Path;
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "hellooo"
+use actix_web::{get, rt, web, App, HttpServer, Responder};
+
+use crate::data::NeuroscopePage;
+
+#[get("/api/solu-1l/neuroscope/{layer_index}/{neuron_index}")]
+async fn index(indices: web::Path<(u32, u32)>) -> impl Responder {
+    let (layer_index, neuron_index) = indices.into_inner();
+    let path = Path::new("data")
+        .join("solu-1l")
+        .join("neuroscope")
+        .join(format!("l{layer_index}n{neuron_index}.postcard",));
+    let page = NeuroscopePage::from_file(path).unwrap();
+    serde_json::to_string(&page).unwrap()
 }
 
 pub fn start_server() -> std::io::Result<()> {
