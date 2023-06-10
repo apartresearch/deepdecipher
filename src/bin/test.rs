9a4b4b39735c6110489db44411322db8878ace77
Albert Garde <albertsgarde@gmail.com>
2023-06-10 16:46:53 +0200

    :fire: Remove test executable

--- src/bin/test.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-
-use neuronav::data::NeuroscopePage;
-use scraper::Html;
-
-const FLOAT_REGEX: &str = r"(-?\d+(?:\.\d*)?)";
-pub fn main() {
-    println!("{}", &format!(r"<h4>Max Range: <b>{FLOAT_REGEX}</b>."));
-}
