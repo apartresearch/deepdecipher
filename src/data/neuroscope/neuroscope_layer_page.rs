6dbc34169eeedb35cc5f8f73de8077ee5b9dbd03
Albert Garde <albertsgarde@gmail.com>
2023-07-20 10:53:58 +0200

    :fire: Remove unnecessary methods from neuroscope pages

--- src/data/neuroscope/neuroscope_layer_page.rs
+++ src/data/neuroscope/neuroscope_layer_page.rs
@@ -1,11 +1,4 @@
-use std::{
-    fs::{self, File},
-    io::{self, BufReader, Read, Write},
-    path::Path,
-};
-
 use anyhow::{Context, Result};
-use flate2::{bufread::DeflateDecoder, write::DeflateEncoder, Compression};
 use serde::{Deserialize, Serialize};
 use snap::raw::{Decoder, Encoder};
 
@@ -52,36 +45,5 @@ impl NeuroscopeLayerPage {
             .context("Failed to decompress neuroscope layer page")?;
         postcard::from_bytes(data.as_slice())
             .context("Failed to deserialize neuroscope layer page.")
-    }
-
-    pub fn to_file<P: AsRef<Path>>(&self, path: P) -> Result<()> {
-        let path = path.as_ref();
-        fs::create_dir_all(
-            path.parent()
-                .with_context(|| format!("Invalid path '{path:?}'"))?,
-        )
-        .with_context(|| format!("Failed to create directory for '{path:?}'"))?;
-        let data = postcard::to_allocvec(&self).context("Failed to serialize neuroscope page.")?;
-
-        let file =
-            File::create(path).with_context(|| format!("Failed to create file '{path:?}'."))?;
-        let mut encoder = DeflateEncoder::new(file, Compression::default());
-        encoder
-            .write_all(&data)
-            .context("Failed to compress neuroscope page.")
-    }
-
-    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let path = path.as_ref();
-        let file = File::open(path).with_context(|| format!("Failed to open file '{path:?}'."))?;
-        let buf_reader = BufReader::new(file);
-        let decoder = DeflateDecoder::new(buf_reader);
-        let data = decoder
-            .bytes()
-            .collect::<io::Result<Vec<u8>>>()
-            .context("Failed to decompress neuroscope page.")?;
-
-        postcard::from_bytes(&data)
-            .with_context(|| format!("Failed to deserialize neuroscope page from file '{path:?}'."))
     }
 }
