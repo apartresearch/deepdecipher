326028eda74c974e0a3bba7aff0c9a6e6e46537a
Albert Garde <albertsgarde@gmail.com>
2023-08-05 00:12:24 +0200

    :sparkles: Add model table to index

---
+++ frontend/js/index.js
@@ -0,0 +1,22 @@
+const modelRow = (model) => {
+    return `<tr>
+        <td><a href="http://localhost:8080/viz/${model.name}/all">${model.name}</a></td>
+        <td>${model.activation_function}</td>
+        <td>${model.dataset}</td>
+        <td>${model.num_layers}</td>
+        <td>${model.layer_size}</td>
+        <td>${model.num_total_neurons}</td>
+        <td>${model.num_total_parameters}</td>
+        <td>${model.available_services.join(", ")}</td>
+    </tr>`
+}
+
+fetch(
+    `${baseUrl}/${baseExtApi}`
+).then(response => response.json()).then(data => {
+    const models = data.models;
+    const table = document.getElementById('model-table');
+    for (model of models) {
+        table.innerHTML += modelRow(model);
+    }
+})
