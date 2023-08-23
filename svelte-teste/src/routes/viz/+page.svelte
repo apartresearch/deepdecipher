7883a2094c1194c45d7844c35c2a287ad7539788
Albert Garde <albertsgarde@gmail.com>
2023-08-23 16:29:43 +0200

    :bug: Fix style bug in deployment

--- svelte-teste/src/routes/viz/+page.svelte
+++ svelte-teste/src/routes/viz/+page.svelte
@@ -22,37 +22,40 @@
 	</tr>
 </table>
 <table id="model-table">
-	<tr>
-		<th>Model</th>
-		<th>Activation Function</th>
-		<th>Dataset</th>
-		<th>Layers</th>
-		<th>Neurons per Layer</th>
-		<th>Total Neurons</th>
-		<th>Total Parameters</th>
-		<th>Available Services</th>
-	</tr>
-
-	{#each models as model}
-		<tr>
-			<td><a href="/{VIZ_EXT}/{model.name}/all">{model.name}</a></td>
-			<td>{model.activationFunction}</td>
-			<td>{model.dataset}</td>
-			<td>{model.numLayers}</td>
-			<td>{model.layerSize.toLocaleString('en-US')}</td>
-			<td>{model.numTotalNeurons.toLocaleString('en-US')}</td>
-			<td>{model.numTotlalParameters.toLocaleString('en-US')}</td>
-			<td>{model.availableServices}</td>
+	<thead>
+		<tr class="model-table-row">
+			<th>Model</th>
+			<th>Activation Function</th>
+			<th>Dataset</th>
+			<th>Layers</th>
+			<th>Neurons per Layer</th>
+			<th>Total Neurons</th>
+			<th>Total Parameters</th>
+			<th>Available Services</th>
 		</tr>
-	{/each}
-	<tbody id="model-table" />
+	</thead>
+
+	<tbody>
+		{#each models as model}
+			<tr class="model-table-row">
+				<td><a href="/{VIZ_EXT}/{model.name}/all">{model.name}</a></td>
+				<td>{model.activationFunction}</td>
+				<td>{model.dataset}</td>
+				<td>{model.numLayers}</td>
+				<td>{model.layerSize.toLocaleString('en-US')}</td>
+				<td>{model.numTotalNeurons.toLocaleString('en-US')}</td>
+				<td>{model.numTotlalParameters.toLocaleString('en-US')}</td>
+				<td>{model.availableServices}</td>
+			</tr>
+		{/each}
+	</tbody>
 </table>
 <div id="tooltip" />
 
 <style>
 	#model-table,
-	#model-table tr th,
-	#model-table tr td {
+	.model-table-row th,
+	.model-table-row td {
 		border: 1px solid;
 	}
 </style>
