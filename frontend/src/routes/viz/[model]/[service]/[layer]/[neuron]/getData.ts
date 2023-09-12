4e2043c12fd378cba7f708f504fda545bf566f2c
Albert Garde <albertsgarde@gmail.com>
2023-09-12 17:49:38 +0200

    :fire: Fix bug in service data URL

--- frontend/src/routes/viz/[model]/[service]/[layer]/[neuron]/getData.ts
+++ frontend/src/routes/viz/[model]/[service]/[layer]/[neuron]/getData.ts
@@ -3,7 +3,7 @@ import { BASE_API_URL, API_EXT, VIZ_EXT } from '$lib/base';
 import { error } from '@sveltejs/kit';
 
 export async function getServiceData(modelName: String, service: String, layerIndex: number, neuronIndex: number) {
-    const url = `${BASE_API_URL}/${API_EXT}/${modelName}/${service}/${layerIndex}/${neuronIndex}`;
+    const url = `/${API_EXT}/${modelName}/${service}/${layerIndex}/${neuronIndex}`;
     const response = await fetch(
         url
     );
