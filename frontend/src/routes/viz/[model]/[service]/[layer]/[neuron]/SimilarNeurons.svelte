454c79d658bed258fe35fde590f347dba071f0c3
Albert Garde <albertsgarde@gmail.com>
2023-09-19 17:23:25 +0200

    :lipstick: Add tooltips to similar neuron links

--- frontend/src/routes/viz/[model]/[service]/[layer]/[neuron]/SimilarNeurons.svelte
+++ frontend/src/routes/viz/[model]/[service]/[layer]/[neuron]/SimilarNeurons.svelte
@@ -1,29 +1,44 @@
 <script lang="ts">
+	import { Tooltip } from '@svelte-plugins/tooltips';
 	import NeuronLink from '$lib/NeuronLink.svelte';
 	import { VIZ_EXT, formatNumber } from '$lib/base';
 
-	export let similarNeurons: any[];
+	export let similarNeurons: { layer: number; neuron: number; similarity: number }[];
 	export let modelName: string;
 
-	$: similarNeurons = similarNeurons.sort((a, b) => b.similarity - a.similarity);
+	const numSimilarNeuronsTableRows = 4;
+
+	function similarNeuronsTableDataFunc(
+		similarNeurons: { layer: number; neuron: number; similarity: number }[]
+	): { layer: number; neuron: number; similarity: number }[][] {
+		const sorted = similarNeurons.sort((a, b) => b.similarity - a.similarity);
+		const numCols = Math.ceil(sorted.length / numSimilarNeuronsTableRows);
+		let tableData = new Array(numSimilarNeuronsTableRows)
+			.fill(null)
+			.map(() => new Array(0).fill(null));
+		for (let i = 0; i < sorted.length; i++) {
+			const row = Math.floor(i / numCols);
+			tableData[row].push(sorted[i]);
+		}
+		return tableData;
+	}
+	$: similarNeuronsTableData = similarNeuronsTableDataFunc(similarNeurons);
 </script>
 
 <div id="similar">
 	{#if similarNeurons.length > 0}
 		<table>
-			<thead>
-				<tr>
-					<th>Similarity</th>
-					<th>Neuron</th>
-				</tr>
-			</thead>
 			<tbody>
-				{#each similarNeurons as { layer, neuron, similarity }}
-					<tr
-						><td>{formatNumber(similarity, 5)}</td><td
-							><NeuronLink {modelName} {layer} {neuron} /></td
-						></tr
-					>
+				{#each similarNeuronsTableData as row}
+					<tr>
+						{#each row as { layer, neuron, similarity }}
+							<td class="neuron"
+								><Tooltip content="Similarity: {formatNumber(similarity, 5)}"
+									><NeuronLink {modelName} {layer} {neuron} /></Tooltip
+								></td
+							>
+						{/each}
+					</tr>
 				{/each}
 			</tbody>
 		</table>
@@ -34,9 +49,13 @@
 
 <style>
 	#similar {
-		text-align: left;
+		text-align: center;
 		display: flex;
 		flex-wrap: wrap;
 		flex-direction: row;
+	}
+
+	.neuron {
+		padding-right: 1.5em;
 	}
 </style>
