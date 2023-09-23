8f4b05896fb1650f56b2c186c65c9b7c5c658658
Albert Garde <albertsgarde@gmail.com>
2023-09-23 09:30:21 +0200

    :truck: Move deployment binaries

--- server/deploy_backend.sh
+++ server/deploy_backend.sh
@@ -1,3 +1,10 @@
-cd /home/albert/deepdecipher;
-cargo build --release;
-sudo systemctl restart deepdecipher-backend;
+# Noop to authenticate sudo immediately
+sudo true
+# Change to the Rust project directory.
+cd /home/albert/deepdecipher
+# Build in release mode.
+/home/albert/.cargo/bin/cargo build --release
+# Copy binary to bin directory.
+sudo cp target/release/server /usr/local/bin/deepdecipher/prod/server
+# Restart the backend service to use the new binary.
+sudo systemctl restart deepdecipher-backend
--- server/deploy_frontend.sh
+++ server/deploy_frontend.sh
@@ -1,3 +1,6 @@
-cd /home/albert/deepdecipher/frontend;
-npm run build;
-sudo systemctl restart deepdecipher;
+# Noop to authenticate sudo.
+sudo true
+cd /home/albert/deepdecipher/frontend
+/usr/local/nodejs/bin/npm install
+/usr/local/nodejs/bin/npm run build
+sudo systemctl restart deepdecipher
