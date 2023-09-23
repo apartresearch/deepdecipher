1b3626c7a774d58f8eee0096f7006ddd20190b47
Albert Garde <albertsgarde@gmail.com>
2023-09-23 10:04:57 +0200

    :truck: Move frontend

--- server/deploy_frontend.sh
+++ server/deploy_frontend.sh
@@ -1,6 +1,12 @@
-# Noop to authenticate sudo.
+# Noop to authenticate sudo immediately
 sudo true
+# Change to the frontend directory in the repo
 cd /home/albert/deepdecipher/frontend
+# Install required packages
 /usr/local/nodejs/bin/npm install
+# Build frontend server
 /usr/local/nodejs/bin/npm run build
+# Copy module to bin directory
+sudo cp -r . /usr/local/bin/deepdecipher/prod/frontend
+# Restart frontend service to use new version
 sudo systemctl restart deepdecipher
