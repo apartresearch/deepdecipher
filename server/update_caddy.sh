fcb8d57696f18128c50d7bc5b66e58b3f1ff9e4f
Albert Garde <albertsgarde@gmail.com>
2023-10-01 12:22:13 +0200

    :sparkles: Find repo path dynamically

--- server/update_caddy.sh
+++ server/update_caddy.sh
@@ -1,8 +1,10 @@
-# Noop to authenticate sudo immediately
+# Hack to get repo location. This breaks if the server directory is moved.
+REPO_PATH=$(dirname $(dirname $(readlink -f $0)))
+# Noop to authenticate sudo immediately.
 sudo true
-# Change to the frontend directory in the repo
-cd /home/albert/deepdecipher
-# Update Caddyfile
+# Change to the repo directory.
+cd $REPO_PATH
+# Update Caddyfile.
 sudo cp --backup=numbered -f server/Caddyfile /etc/caddy/Caddyfile
-# Reload Caddy with new configuration
+# Reload Caddy with new configuration.
 caddy reload --config /etc/caddy/Caddyfile
